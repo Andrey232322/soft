cd5a8bd8cda4f46ef495200ffa1900f20dc55cc9
unknown <dwigatel3532@gmail.com>
2023-07-10 20:54:37 +0400

    zadatki tg bota

--- telegbot.py
+++ telegbot.py
@@ -1,5 +1,7 @@
-import telebot
+import tempfile
 
+import telebot
+from skill import *
 
 bot = telebot.TeleBot('5868508351:AAHDjQ1D9uYfxhckwCgJwEBgoq4q3dk3Exw')
 
@@ -9,13 +11,34 @@ bot = telebot.TeleBot('5868508351:AAHDjQ1D9uYfxhckwCgJwEBgoq4q3dk3Exw')
 def statrt(message):
     if message.text == 'Привет':
         bot.send_message(message.from_user.id, 'ну привет')
+    if message.text == 'картинка':
+        file = open('23.jpg', 'rb')
+        bot.send_photo(message.chat.id, file)
+    if message.text == 'прибавь звук':
+        bot.send_message(message.chat.id, 'прибавил звук')
+        soundup10()
     else:
-        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+        bot.send_message(message.from_user.id, 'я не знаю чего ты хочешь')
+
+@bot.message_handler(regexp='прибавить')
+def echo_message(message):
+    bot.send_message(message.chat.id,'прибавил звук')
+    soundup10()
 
 
 
-bot.polling(none_stop=True,interval=0)
+
+@bot.message_handler(content_types=["text"])
+def echo_message_png(message):
+    if message.text == 'картинка':
+        file = open('23.jpg', 'rb')
+        bot.send_photo(message.chat.id, file)
+    else:
+        bot.send_message(message.from_user.id, 'я не знаю что ты хочешь')
 
 
-if __name__ == '__main__':
-    statrt()
+
+bot.infinity_polling()
+
+# if __name__ == '__main__':
+#     statrt()
