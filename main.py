930eb08ac3956199bc84f2bb08e2cbf3d424b660
unknown <dwigatel3532@gmail.com>
2023-07-10 14:53:18 +0400

    update main

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import sys
 import sounddevice as sd
 import vosk
 import json
+import words
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+from skill import *
+
 
 q = queue.Queue()
 
@@ -11,20 +16,45 @@ model = vosk.Model('model_small')
 
 device = sd.default.device
 samplerate = int(sd.query_devices(device[0], 'input')['default_samplerate'])
+
 def callback(indata, frames, time, status):
 
     q.put(bytes(indata))
 
+def reco(data,vectorizer,clf):
+    trg =  words.Triger.intersection(data.split())
+    if not trg:
+        return
+    data.replace(list(trg)[0],'')
+    text_vector = vectorizer.transform([data]).toarray()[0]
+    answer = clf.predict([text_vector])[0]
 
-with sd.RawInputStream(samplerate=samplerate, blocksize=40000, device=device[0],
-                           dtype="int16", channels=1, callback=callback):
+    func = answer.split()[0]
+    exec(func + '()')
 
 
-    rec = vosk.KaldiRecognizer(model,samplerate)
-    while True:
-        data = q.get()
-        if rec.AcceptWaveform(data):
-            a = json.loads(rec.Result())['text']
-            print(a)
-        # else:
-        #     print(rec.PartialResult())
+
+
+
+def main():
+    vectorizer = CountVectorizer()
+    vectors = vectorizer.fit_transform(list(words.data_set.keys()))
+
+    clf = LogisticRegression()
+    clf.fit(vectors,list(words.data_set.values()))
+
+    del words.data_set
+
+    with sd.RawInputStream(samplerate=samplerate, blocksize=40000, device=device[0],
+                               dtype="int16", channels=1, callback=callback):
+
+
+        rec = vosk.KaldiRecognizer(model,samplerate)
+        while True:
+            data = q.get()
+            if rec.AcceptWaveform(data):
+                a = json.loads(rec.Result())['text']
+                reco(a,vectorizer,clf)
+
+if __name__ =='__main__':
+    main()
